d7767d5ef2fdc4e4eca1aa86e28cca60edf75270
Nate Wessels <nwessels16@gmail.com>
2019-04-09 19:23:32 -0700

    NEws api

---
+++ news-api/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
